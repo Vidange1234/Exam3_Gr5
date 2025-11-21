55f9e3949e6cd6b1ef5b10ac923404df636d523c
Fred <2366548@cegepoutaouais.qc.ca>
2025-11-21 13:37:40 -0500

    correction tests

--- Test_Q2.py
+++ Test_Q2.py
@@ -5,13 +5,23 @@ from rich.repr import Result
 from Q2 import afficher_jours_examens
 
 
-def test_afficher_jours_examens(horaire_examen):
+def test_afficher_jours_1():
+    #arrange
     horaire_examen = {
         "math": "10/12/2015",
         "anglais": "12/12/2025",
         "français": "15/12/2025"
 
     }
-    resultat_attendu="jeudi", "vendredi", "lundi"
-    assert afficher_jours_examens(horaire_examen) == resultat_attendu
-    assert date = datetime.datetime.strptime(horaire_examen[examen], "%d/%m/%Y")
+    resultat_attendu=["jeudi", "vendredi", "lundi"]
+    #act
+    resultat = afficher_jours_examens(horaire_examen)
+    #assert
+    assert resultat == resultat_attendu
+def test_afficher_jours2():
+    #act
+    horaire_examen = {}
+    resultat_att = []
+    
+    
+   
